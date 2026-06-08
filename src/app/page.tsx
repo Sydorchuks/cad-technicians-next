af9ec6e411f9887fbec3a60e6aa3fb518b7a4e4e
Sydorchuks <77363409+Sydorchuks@users.noreply.github.com>
2026-06-08 17:28:21 +0300

    feat: add video section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import HomeAbout from "./components/HomeAbout/HomeAbout";
 import HomeHero from "./components/HomeHero/HomeHero";
 import HomeHowItWorks from "./components/HomeHowItWorks/HomeHowItWorks";
+import Video from "./components/Video/Video";
 
 export default function Home(){
     return(<>
@@ -9,5 +10,6 @@ export default function Home(){
         <HomeHero />
         <HomeAbout />
         <HomeHowItWorks />
+        <Video />
     </>)
 }
