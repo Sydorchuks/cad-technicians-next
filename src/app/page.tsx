deb18a75cc861db5bfe1db97c991128d026ad4d1
Oleh Sydorchuk <olegsydorchuk9@gmail.com>
2026-06-16 11:57:27 +0300

    Improved App router logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 import ContactBlock from "./components/ContactBlock/ContactBlock";
 import Feedback from "./components/Feedback/Feedback";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import HomeAbout from "./components/HomeAbout/HomeAbout";
 import HomeHero from "./components/HomeHero/HomeHero";
 import HomeHowItWorks from "./components/HomeHowItWorks/HomeHowItWorks";
 import Video from "./components/Video/Video";
-import DarkLayout from "./darkLayout";
+import PageLayout from "./pageLayout";
 
 export default function Home(){
     return(
-        <DarkLayout>
+        <PageLayout>
             <HomeHero />
             <HomeAbout />
             <HomeHowItWorks />
             <Video />
             <Feedback />
             <ContactBlock />
-        </DarkLayout>
+        </PageLayout>
     )
 }
