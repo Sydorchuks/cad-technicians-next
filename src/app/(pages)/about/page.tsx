47efcc8cac828a1fb54790c4c74895f42bf7f7b2
Oleh Sydorchuk <olegsydorchuk9@gmail.com>
2026-06-16 16:31:53 +0300

    Actual changes to about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,25 +1,13 @@
 import ContactBlock from "@/app/components/ContactBlock/ContactBlock";
 import Feedback from "@/app/components/Feedback/Feedback";
 import GenericHero from "@/app/components/GenericHero/GenericHero";
+import { aboutHero } from "@/app/content/heroes";
 import PageLayout from "@/app/pageLayout";
 
 export default function About(){
     return(
         <PageLayout>
-            <GenericHero
-                bgText="ABOUT US"
-                title="ABOUT US"
-                description={
-                    <>
-                        Survey Drafters has decades of experience in the land surveying industry,
-                        and our ability to work virtually with professionals across the country is what sets us apart from the rest.
-                        We minimize your expenses and maximize your productivity by using your field data to create high-quality AutoCAD drawings.
-                        You can take on more clients and promise a faster turnaround time to your clients by partnering{" "}
-                        <br className="hidden xl:block" />
-                        with Survey Drafters.
-                    </>
-                }
-            />
+            <GenericHero {...aboutHero} />
             <Feedback />
             <ContactBlock />
         </PageLayout>
