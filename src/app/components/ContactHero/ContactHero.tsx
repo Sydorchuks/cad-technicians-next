f42e92244a020c55ead4c2dd57e36f58155c3771
Oleh Sydorchuk <olegsydorchuk9@gmail.com>
2026-06-19 17:43:35 +0300

    changes according to feedback

--- src/app/components/ContactHero/ContactHero.tsx
+++ src/app/components/ContactHero/ContactHero.tsx
@@ -4,32 +4,23 @@ export default function ContactHero() {
   return (
     <section className="relative h-96.5 w-full overflow-hidden bg-white md:h-130 lg:h-155 xl:h-180 2xl:h-197.5 desktop:h-219.75">
       <Container className="relative h-full">
-        <div className="absolute left-3.5 md:left-0 top-29.25 z-10 md:top-37.5 lg:top-47.5 xl:top-58.75 2xl:top-67.5 desktop:left-11.5 desktop:top-73">
-          <h2 className="font-lora text-2xl font-normal uppercase leading-none text-text-dark 
-              md:text-[40px] lg:text-[76px] xl:text-[92px] 2xl:text-[108px] desktop:text-9xl desktop:leading-38.5"
-          >
+        <div className="relative z-10 pt-29.25 pl-3.5 md:pt-37.5 md:pl-0 lg:pt-47.5 xl:pt-58.75 2xl:pt-67.5 desktop:pt-73 desktop:pl-11.5">
+          <h2 className="font-lora text-2xl font-normal uppercase leading-none text-text-dark md:text-[40px] lg:text-[76px] xl:text-[92px] 2xl:text-[108px] desktop:text-9xl desktop:leading-38.5">
             CONTACT US
           </h2>
 
-          <p className="mt-5 w-57.25 font-body text-[14px] font-normal leading-4.5 md:leading-6.5
-             text-text-dark md:w-80.5 md:text-base lg:w-125 xl:w-152.5 xl:text-md 2xl:w-175 desktop:w-188.25"
-          >
+          <p className="mt-5 w-57.25 font-body text-[14px] font-normal leading-4.5 text-text-dark md:w-80.5 md:text-base md:leading-6.5 lg:w-125 xl:w-152.5 xl:text-md 2xl:w-175 desktop:w-188.25">
             Getting started with us couldn’t be easier, but we understand if you have a few questions.
             Feel free to reach out and let us know how we can help.
           </p>
 
-          <h3 className="mt-9 font-lora text-md md:text-[30px] lg:text-[35px] font-bold uppercase 
-              leading-none text-text-dark md:mt-16.25 lg:mt-32 xl:mt-36 2xl:mt-32 desktop:mt-24"
-          >
+          <h3 className="mt-9 font-lora text-md font-bold uppercase leading-none text-text-dark md:mt-16.25 md:text-[30px] lg:mt-32 lg:text-[35px] xl:mt-36 2xl:mt-32 desktop:mt-24">
             EMAIL
           </h3>
 
           <a
             href="mailto:support@cadtechnicians.com"
-            className="md:mt-2.25 xl:mt-4.25 block w-fit font-body text-[16px] font-normal 
-              leading-6.5 text-primary transition-opacity hover:opacity-70 md:text-[20px] 
-              md:leading-9 lg:text-[32px] lg:leading-10.5 xl:text-[36px] xl:leading-11.5 
-              2xl:text-[30px] 2xl:leading-10 desktop:text-md desktop:leading-6.5"
+            className="block w-fit font-body text-[16px] font-normal leading-6.5 text-text-dark transition-opacity hover:opacity-70 md:mt-2.25 md:text-[20px] md:leading-9 lg:text-[32px] lg:leading-10.5 xl:mt-4.25 xl:text-[36px] xl:leading-11.5 2xl:text-[30px] 2xl:leading-10 desktop:text-md desktop:leading-6.5"
           >
             support@cadtechnicians.com
           </a>
@@ -38,10 +29,7 @@ export default function ContactHero() {
         <img
           src="/contact-hero-img.svg"
           alt=""
-          className="absolute scale-[2] md:scale-100 left-90 -top-14 z-0 h-96.5 w-90 object-contain
-            opacity-70 md:left-62.5 md:top-20 md:h-110 md:w-158.25 lg:left-130 lg:top-23.75 lg:h-130 lg:w-180
-            xl:left-160 xl:top-20 xl:h-160 xl:w-205 2xl:left-190 2xl:top-8.75 2xl:h-195 2xl:w-217.5 
-            desktop:left-215.5 desktop:top-5.25 desktop:h-214.5 desktop:w-225.25 desktop:opacity-100"
+          className="absolute left-90 -top-14 z-0 h-96.5 w-90 scale-[2] object-contain opacity-70 md:left-62.5 md:top-20 md:h-110 md:w-158.25 md:scale-100 lg:left-130 lg:top-23.75 lg:h-130 lg:w-180 xl:left-160 xl:top-20 xl:h-160 xl:w-205 2xl:left-190 2xl:top-8.75 2xl:h-195 2xl:w-217.5 desktop:left-215.5 desktop:top-5.25 desktop:h-214.5 desktop:w-225.25 desktop:opacity-100"
         />
       </Container>
     </section>
--- src/app/components/WhatWeDo/WhatWeDo.tsx
+++ src/app/components/WhatWeDo/WhatWeDo.tsx
@@ -3,29 +3,32 @@ import "../../styles/vertical-lines.css";
 
 export default function WhatWeDo() {
   return (
-    <section className="relative h-127.5 w-full overflow-hidden bg-white md:h-177.75 xl:h-307.5">
-      <div className="absolute left-1/2 top-35 md:top-47.5 z-10 w-full max-w-79.25 -translate-x-1/2 text-center md:max-w-124.25 xl:left-[50%] xl:top-65 xl:w-230 xl:max-w-none">
+    <section className="relative flex h-125 w-full flex-col overflow-hidden bg-white md:h-177.75 xl:h-307.5">
+      <div className="relative z-10 mx-auto mt-23 w-full max-w-79.25 text-center md:mt-47.5 md:max-w-124.25 xl:mt-65 xl:w-230 xl:max-w-none">
         <BackgroundTitle
           variant="light"
           className="
-            left-1/2 -top-35 w-full -translate-x-1/2 text-center
+            left-1/4 md:left-1/2 -top-23 w-full -translate-x-1/2 text-center
             text-bg-sm leading-[0.9] whitespace-normal opacity-66
             md:-top-50 md:text-bg-md
-            xl:left-300 2xl:left-275 xl:-top-51.25 xl:w-auto xl:text-bg-lg xl:leading-none xl:whitespace-nowrap
+            xl:left-300 xl:-top-51.25 xl:w-auto xl:text-bg-lg xl:leading-none xl:whitespace-nowrap
+            2xl:left-275
           "
         >
           <span className="block md:hidden">WHAT WE</span>
-          <span className="block md:hidden">DO?</span>
+          <span className="block translate-x-1/4 md:hidden">DO?</span>
 
           <span className="hidden md:block xl:inline">WHAT </span>
-          <span className="hidden md:block md:-ml-13 xl:ml-0 xl:inline">WE DO?</span>
+          <span className="hidden md:block md:-ml-13 xl:ml-0 xl:inline">
+            WE DO?
+          </span>
         </BackgroundTitle>
 
-        <h2 className="relative -top-12 md:top-0 z-10 font-lora text-4xl font-normal uppercase leading-none text-text-dark md:text-7xl xl:text-9xl">
+        <h2 className="relative z-10 mt-0 font-lora text-4xl font-normal uppercase leading-none text-text-dark md:text-7xl xl:text-9xl">
           WHAT WE DO?
         </h2>
 
-        <p className="relative z-10 mx-auto mt-5 w-full font-body text-base font-normal leading-6.5 text-text-dark md:mt-7 md:text-md xl:mt-7.5 xl:w-175">
+        <p className="relative z-10 mx-auto mt-18 w-full font-body text-base font-normal leading-6.5 text-text-dark md:mt-7 md:text-md xl:mt-7.5 xl:w-175">
           Our business is a blend of technical knowledge and experienced customer service. From start to finish, you&apos;ll find that working
           with Survey Drafters is a pleasure. We offer a partnership that allows your business to skyrocket to the next level.
         </p>
@@ -34,7 +37,7 @@ export default function WhatWeDo() {
       <img
         src="/what-we-do.svg"
         alt=""
-        className="absolute bottom-0 left-0 z-10 h-32.25 w-full object-cover md:h-68.5 xl:h-167"
+        className="mt-auto h-32.25 w-full object-cover md:h-68.5 xl:h-167"
       />
     </section>
   );
